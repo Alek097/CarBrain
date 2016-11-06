0874c5aedb5019b9d2ce94e7b6467279e32d580a
Alek097 <petr19972510>
2016-11-06 21:03:09 +0300

    Initial commit: added Startup file

---
+++ CarBrain/Startup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using CarBrain.System;
+
+namespace CarBrain
+{
+	public static class Startup
+	{
+		public static void RegisterSetting(params ISetting[] settings)
+		{
+			foreach (ISetting setting in settings)
+			{
+				setting.Start ();
+			}
+		}
+
+		public static void RegisterServices(params IService[] services)
+		{
+			foreach (IService service in services)
+			{
+				Task.Run (() => {
+					bool isNext = true;
+
+					while(isNext)
+					{
+						isNext = service.Loop();
+					}
+				});
+			}
+		}
+	}
+}
+
