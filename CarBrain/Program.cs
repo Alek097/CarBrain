ddf0ac07424a079aa66c2ba60a011541a68ea4cb
Alek097 <petr19972510>
2016-11-06 15:18:22 +0300

    Initial commit

---
+++ CarBrain/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Gtk;
+
+namespace CarBrain
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Application.Init ();
+			MainWindow win = new MainWindow ();
+			win.Fullscreen ();
+			Application.Run ();
+		}
+	}
+}
