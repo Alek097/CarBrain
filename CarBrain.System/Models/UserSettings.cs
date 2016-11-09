6d8c835ac33ba86a2bf9d1727251b1330998b196
Alek097 <petrovskii097@gmail.com>
2016-11-09 20:07:23 +0300

    Initial commit: added UserSettings

---
+++ CarBrain.System/Models/UserSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using MongoDB.Bson;
+
+namespace CarBrain.System.Models
+{
+	internal class UserSettings
+	{
+		public ObjectId Id { get; set; }
+		public byte[] Background { get; set; }
+		public List<Icon> Icons { get; set; }
+	}
+}
+
