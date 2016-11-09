06ff34aad37f1b60e0393777e08a3f509d605b08
Alek097 <petrovskii097@gmail.com>
2016-11-09 20:50:54 +0300

    Initial commit: added logic in constructor and replace security on private

--- CarBrain.System/Models/Screen.cs
+++ CarBrain.System/Models/Screen.cs
@@ -1,16 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using descktop = System.Windows.Forms.Screen;
+using System.Drawing;
 
 namespace CarBrain.System.Models
 {
 	public class Screen
 	{
-		public int Width { get; internal set; }
-		public int Height { get; internal set; }
+		public int Width { get; private set; }
+		public int Height { get; private set; }
 		public byte[] Background { get; set; }
 		public List<Icon> Icons { get; set; }
 
-		internal Screen() {}
+		internal Screen() 
+		{
+			Rectangle descktopSize = descktop.PrimaryScreen.Bounds;
+
+			this.Width = descktopSize.Width;
+			this.Height = descktopSize.Height;
+		}
 	}
 }
 
