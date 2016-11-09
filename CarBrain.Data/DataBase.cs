6bf877556c05f65e29612b67bd41feacac61fce7
Alek097 <petrovskii097@gmail.com>
2016-11-09 14:06:58 +0300

    Initial commit: added insert methods

--- CarBrain.Data/DataBase.cs
+++ CarBrain.Data/DataBase.cs
@@ -1,11 +1,39 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace CarBrain.Data
 {
-	public class DataBase
+	public class DataBase<TModel>
 	{
-		public DataBase ()
+		private IMongoCollection<TModel> collection { get; set; }
+
+		public DataBase (string dbName, string collectionName)
 		{
+			MongoClient client = new MongoClient ("mongodb://localhost:27017");
+			this.collection = client.GetDatabase (dbName).GetCollection<TModel>(collectionName);
+		}
+
+		public void Insert(TModel model)
+		{
+			this.collection.InsertOne (model);
+		}
+
+		public void Insert(IEnumerable<TModel> models)
+		{
+			this.collection.InsertMany (models);
+		}
+
+		public async void InsertAsync(IEnumerable<TModel> models)
+		{
+			await this.collection.InsertManyAsync (models);
+		}
+
+		public async void InsertAsync(TModel model)
+		{
+			await this.collection.InsertOneAsync (model);
 		}
 	}
 }
