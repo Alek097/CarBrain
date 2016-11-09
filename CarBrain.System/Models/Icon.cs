6c3b60b2cbce69458fe7fc33db2bf6356cd506e7
Alek097 <petrovskii097@gmail.com>
2016-11-09 22:04:12 +0300

    Initial commit: added ids

--- CarBrain.System/Models/Icon.cs
+++ CarBrain.System/Models/Icon.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using MongoDB.Bson;
 
 namespace CarBrain.System.Models
 {
 	public class Icon
 	{
+		public ObjectId Id { get; set; }
 		public Position Position { get; set; }
 		public byte[] Img { get; set; }
 		public string AppPath { get; set; }
