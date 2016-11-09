36403938de915145fb044f35699dbd3e13caf593
Alek097 <petrovskii097@gmail.com>
2016-11-09 21:10:40 +0300

    Initial commit: added property and logic in constructor

--- CarBrain.System/Models/SystemInformation.cs
+++ CarBrain.System/Models/SystemInformation.cs
@@ -1,11 +1,24 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Reflection;
 
 namespace CarBrain.System.Models
 {
 	public class SystemInformation
 	{
 		public Screen Screen { get; internal set; }
-		internal SystemInformation() {}
+
+		public string OS { get; private set; }
+
+		public string Version { get; private set; }
+
+		internal SystemInformation() 
+		{
+			this.OS = Environment.OSVersion.ToString ();
+
+			Assembly assembly = Assembly.GetExecutingAssembly();
+			this.Version = FileVersionInfo.GetVersionInfo(assembly.Location).FileVersion;
+		}
 	}
 }
 
