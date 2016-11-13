b2230c65edf36f1fabfdf936fe7ed4c439cc1265
Alek097 <petrovskii097@gmail.com>
2016-11-14 01:04:11 +0300

    Initial commi: added CreateVaribleCommand, LizardType and reference to System.Numerics

---
+++ Lizard/Model/Reales/CreateVariableCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Numerics;
+
+namespace Lizard.Model.Reales
+{
+	public class CreateVariableCommand<TValue> : Command
+	{
+		public string Name { get; set; }
+		public TValue Value { get; set; }
+		public LizardType Type { get; set; }
+
+		public CreateVariableCommand (string Name, TValue value)
+			:base(CommandType.CreateVariable)
+		{
+			this.Name = Name;
+			this.Value = value;
+
+			Type valueType = typeof(TValue);
+
+			if (valueType == typeof(BigInteger)) 
+			{
+				this.Type = LizardType.Numeric;
+			} 
+			else if (valueType == typeof(string)) 
+			{
+				this.Type = LizardType.String;
+			} 
+			else if (valueType == typeof(bool)) 
+			{
+				this.Type = LizardType.Boolean;
+			} 
+			else 
+			{
+				this.Type = LizardType.Custom;
+			}
+		}
+
+		public CreateVariableCommand ()
+		{
+			
+		}
+	}
+}
+
---
+++ Lizard/Model/Reales/LizardType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lizard.Model.Reales
+{
+	public enum LizardType
+	{
+		Numeric,
+		Boolean,
+		String,
+		Custom
+	}
+}
+
